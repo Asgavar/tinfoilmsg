9f71995efc9a889533dc6cf2a9ef9fa54bf9d064
Artur Juraszek <asgavar@gmail.com>
2018-01-12 11:17:22 +0100

    Enable ASCII validation

--- libstegan/libstegan.py
+++ libstegan/libstegan.py
@@ -12,6 +12,14 @@ def decode():
     pass  # TODO
 
 
+def _validate_ascii(message):
+    """
+    Ensure that the message provided is composed of ASCII letters only,
+    it's crucial since it will be mapped to RGB values which can't exceed 255.
+    """
+    return all(ord(c) < 128 for c in message)
+
+
 class RGBCycler:
     def __init__(conf_dict):
         pass
--- libstegan/test/test_libstegan.py
+++ libstegan/test/test_libstegan.py
@@ -3,3 +3,11 @@ import libstegan
 
 def test_imported_correctly():
     assert 'RGBCycler' in libstegan.__dict__
+
+def test_ascii_validation_true():
+    msg = 'An ascii-only string :) ;>'
+    assert libstegan._validate_ascii(msg) == True
+
+def test_ascii_validation_false():
+    msg = 'Żażółć gęślą żółtą jaźń'
+    assert libstegan._validate_ascii(msg) == False
