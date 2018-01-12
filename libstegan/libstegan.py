7cb337b3fee8d296c10189fce69ef5db08db2336
Artur Juraszek <asgavar@gmail.com>
2018-01-12 11:47:36 +0100

    Enable calculating the min. pixel amount for a given message

--- libstegan/libstegan.py
+++ libstegan/libstegan.py
@@ -1,6 +1,16 @@
 """
 A small helper library for tinfoilmsg which handles the image and text
 transformations.
+The dictionary which is supposed to be passed to these functions and classes
+can look like this:
+{
+    'red': True,
+    'green': False,
+    'blue': True,
+    'frequency': 3
+}
+This will tell them that only red and blue channels are interesting, and pixels
+on positions dividable by 3 in particular.
 """
 
 
@@ -18,6 +28,16 @@ def _validate_ascii(message):
     it's crucial since it will be mapped to RGB values which can't exceed 255.
     """
     return all(ord(c) < 128 for c in message)
+
+
+def _minimal_pixel_count(conf_dict, message):
+    """
+    Calculates the minimal amount of pixels needed to encode the message.
+    """
+    # let's assume that the red, green and blue are the only bools there
+    meaningful_channels_amount = list(conf_dict.values()).count(True)
+    frequency = conf_dict['frequency']
+    return len(message) + ((frequency - 1) * (len(message) - 1))
 
 
 class RGBCycler:
--- libstegan/test/test_libstegan.py
+++ libstegan/test/test_libstegan.py
@@ -1,6 +1,14 @@
 import libstegan
 
 
+generic_conf_dict =  {
+    'red': True,
+    'green': False,
+    'blue': True,
+    'frequency': 3
+}
+
+
 def test_imported_correctly():
     assert 'RGBCycler' in libstegan.__dict__
 
@@ -11,3 +19,8 @@ def test_ascii_validation_true():
 def test_ascii_validation_false():
     msg = 'Żażółć gęślą żółtą jaźń'
     assert libstegan._validate_ascii(msg) == False
+
+def test_minimal_pixel_count_for_msg_len_5_and_freq_3():
+    message = 'abcde'
+    # I've done it by hand on a sheet of paper so I hope that's correct
+    assert libstegan._minimal_pixel_count(generic_conf_dict, message) == 13
