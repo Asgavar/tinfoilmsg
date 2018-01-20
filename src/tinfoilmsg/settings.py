50468b5d165b09faaa09621c8ca1a47546c202a3
Artur Juraszek <asgavar@gmail.com>
2018-01-20 15:34:35 +0100

    Expose at least three vulnerabilities

--- src/tinfoilmsg/settings.py
+++ src/tinfoilmsg/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tinfoilmsg',
-        'User': 'postgres'
+        'User': 'postgres'  # much security xD
     }
 }
 
@@ -91,10 +91,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        # 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        # 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
