a98f9fc0d53fc0e40e3966a49b10a891496fb720
Artur Juraszek <asgavar@gmail.com>
2018-01-23 10:44:03 +0100

    Fix one test, break another

--- src/core/tests.py
+++ src/core/tests.py
@@ -8,9 +8,9 @@ class AuthenticationTests(TestCase):
     """
     def setUp(self):
         self.client = Client()
-        self.user_name = 'user'
+        self.user_name = 'test_user'
         # also assures that NumericPasswordValidator is off
-        self.user_password = '1234'
+        self.user_password = 'verylong_password1234'
 
     def test_signup_does_not_produce_errors(self):
         response = self.client.post('/signup',data={
@@ -21,8 +21,24 @@ class AuthenticationTests(TestCase):
         self.assertEqual(self.client.errors.read(1024), b'')
 
     def test_login_does_not_produce_errors(self):
-        response = self.client.post('/signup', data={
+        response = self.client.post('/login', data={
             'is_username': self.user_name,
             'id_password': self.user_password
         })
         self.assertEqual(self.client.errors.read(1024), b'')
+
+    # def test_signup_login_end_to_end(self):
+    #     """
+    #     Creates an account, log it in and checks if there is a logout button
+    #     the presence of which indicates the login was succesfull.
+    #     """
+    #     self.client.post('/signup',data={
+    #         'id_username': 'another_user',
+    #         'id_password1': 'anotherpassword'
+    #     })
+    #     self.client.post('/login',data={
+    #         'id_username': 'another_user',
+    #         'id_password': 'anotherpassword'
+    #     })
+    #     response = self.client.get('/')
+    #     self.assertContains(response, 'Log out')
